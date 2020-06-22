c4b8055be579e0f40217e7f9a7fb74ee0b61a909
Marko Stojanovic <marko.stojanovic1994@gmail.com>
2020-06-22 12:36:40 +0200

    Formating

--- src/api/api.js
+++ src/api/api.js
@@ -1,34 +1,14 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const url = 'http://cors-anywhere.herokuapp.com/http://api.tvmaze.com/shows';
+const url = 'http://cors-anywhere.herokuapp.com/http://api.tvmaze.com/shows'
 
 export const fetchData = async (showNumber) => {
-	try {
-		const apiPromise = await axios.get(url);
-		const jsonData = apiPromise.data
+  try {
+    const apiPromise = await axios.get(url)
+    const jsonData = apiPromise.data
 
-		return jsonData
-	} catch (error) {
-		console.log(error)
-	}
-};
-
-// export const fetchDailyData = async () => {
-// 	try {
-// 		const { data } = await axios.get(`${url}/daily`);
-
-// 		const modifiedData = data.map(dailyData => ({
-// 			confirmed: dailyData.confirmed.total,
-// 			deaths: dailyData.deaths.total,
-// 			date: dailyData.reportDate
-// 		}));
-
-// 		return modifiedData;
-// 	} catch (error) {}
-// };
-// export const fetchCountries = async () => {
-// 	try {
-// 		const { data: { countries } } = await axios.get(`${url}/countries`);
-// 		return countries.map(country => country.name);
-// 	} catch (error) {}
-// };
+    return jsonData
+  } catch (error) {
+    console.log(error)
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -4,33 +4,33 @@ import Header from './Header';
 import { fetchData } from '../api/api';
 
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showNames: [ 'Under the Dome' ],
-			showImg: []
-		};
-	}
-	componentDidMount() {
-		fetchData().then(res => {
-			console.log(res);
-			for (let i = 0; i < 10; i++) {
-				this.setState({
-					showNames: [ ...this.state.showNames, res[i].name ]
-				});
-			}
-		});
-	}
-	render() {
-		return (
-			<div>
-				<Header />
-				<h1>Hello React</h1>
-				<h2>{this.state.showNames}</h2>
-				<img src={this.state.showImg} alt="showname" />
-			</div>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      showNames: [],
+      showImg: []
+    };
+  }
+  componentDidMount() {
+    fetchData().then(res => {
+      console.log(res);
+      for (let i = 0; i < 10; i++) {
+        this.setState({
+          showNames: [ ...this.state.showNames, res[i].name ]
+        });
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Hello React</h1>
+        <h2>{this.state.showNames}</h2>
+        <img src={this.state.showImg} alt="showname" />
+      </div>
+    );
+  }
 }
 
 export default App;
